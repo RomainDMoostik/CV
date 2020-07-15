5086daf92833c08d22b0f5c35814961d29ddae03
Romain <romaindigue@gmail.com>
2020-07-15 21:52:42 +0200

    Modifs texte & blocs + ajout media queries

---
+++ CV-RomainDigue/CV-RomainDigue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CV_RomainDigue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
